39b03d58ca8020d27087523f37fb34840a80f4eb
hedongfeng <hedongfeng@qingting.fm>
2019-07-18 15:07:13 +0800

    m

--- analysis_sparse.py
+++ analysis_sparse.py
@@ -130,9 +130,13 @@ if __name__ == "__main__":
         feature_chi = chi2(data_array, label_list)[0]
 
     # 特征拼接
-    feature_comb_list = [feature_comb_0, feature_comb_1, feature_comb_2, feature_comb_3, feature_comb_4,
-                         feature_comb_5, feature_comb_6, feature_comb_7, feature_comb_8]
-    if True:
+    if False:
+        feature_comb_list = [feature_comb_0, feature_comb_1, feature_comb_2, feature_comb_3, feature_comb_4,
+                             feature_comb_5, feature_comb_6, feature_comb_7, feature_comb_8]
+    else:
+        feature_comb_list = [feature_comb_0, feature_comb_1, feature_comb_2, feature_comb_3,
+                             feature_comb_5, feature_comb_6, feature_comb_7, feature_comb_8]
+    if False:
         # 特征交叉
         feature_cate_num = len(feature_comb_list)
         for i in range(0, feature_cate_num - 1):
