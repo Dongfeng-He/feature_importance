d63ce752d98c75b8f7916e13e24a0acd9b801651
hedongfeng <hedongfeng@qingting.fm>
2019-07-18 18:57:42 +0800

    m

--- analysis_dense.py
+++ analysis_dense.py
@@ -172,7 +172,7 @@ if __name__ == "__main__":
 
     if True:
         [learning_rate, n_estimators, max_depth, min_child_weight, gamma, subsample, colsample_bytree, reg_alpha,
-         reg_lambda] = [0.01, 95, 3, 11, 0.5650788634276807, 0.7, 0.9, 0.1, 0.1]
+         reg_lambda] = [0.1, 292, 3, 10, 0.49370939396193736, 0.7, 0.6, 1, 3]
         if os.path.exists("/root/feature_importance"):
             classifier = xgboost.XGBClassifier(n_jobs=-1, random_state=0, seed=10, learning_rate=learning_rate,
                                                n_estimators=n_estimators, max_depth=max_depth,
